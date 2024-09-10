47c5f9e4143a662c43c4ecc0595e39bb97bd464e
Kushal3502 <kushal2002personal@gmail.com>
2024-09-10 14:53:30 +0530

    file upload

---
+++ backend/src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+// middleware to temporarily store the files to the local server
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname + "-" + uniqueSuffix);
+  },
+});
+
+export const upload = multer({ storage: storage });
---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    
+    // upload the file from local server to cloudinary
+    const response = await cloudinary.uploader
+      .upload(localFilePath, { resource_type: "auto" })
+      .catch((error) => {
+        console.log("CLOUDINARY upload ERROR : ", error);
+      });
+
+    console.log(response);
+
+    // delete file from local server
+    fs.unlinkSync(localFilePath);
+
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    console.log("CLOUDINARY upload ERROR : ", error);
+    return null;
+  }
+};
+
+export default uploadOnCloudinary;
