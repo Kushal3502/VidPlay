bd60af7096637c539292391a8d20db0a0f6d7917
Kushal3502 <kushal2002personal@gmail.com>
2024-09-23 20:18:50 +0530

    playlist controller

--- backend/src/controllers/playlist.controller.js
+++ backend/src/controllers/playlist.controller.js
@@ -21,7 +21,9 @@ const createPlaylist = asyncHandler(async (req, res) => {
 
   if (!newPlaylist) throw new ApiError(400, "Something went wrong!!!");
 
-  return res.status(201).json(new ApiResponse(200, newPlaylist));
+  return res
+    .status(201)
+    .json(new ApiResponse(200, newPlaylist, "Playlist created..."));
 });
 
 // get user playlists
@@ -35,6 +37,8 @@ const getUserPlaylists = asyncHandler(async (req, res) => {
   const userPlaylists = await Playlist.find({
     owner: new mongoose.Types.ObjectId(userId),
   });
+
+  if (!userPlaylists) throw new ApiError(400, "Something went wrong!!!");
 
   return res.status(200).json(new ApiResponse(200, userPlaylists));
 });
@@ -62,7 +66,7 @@ const addVideoToPlaylist = asyncHandler(async (req, res) => {
 
   if (!video) throw new ApiError(404, "Video not found!!!");
 
-  if (!playlist?.owner.equals(req?.user._id))
+  if (!playlist?.owner.equals(req.user?._id))
     throw new ApiError(401, "Unauthorized request!!!");
 
   const updatedPlaylist = await Playlist.findByIdAndUpdate(
@@ -77,23 +81,82 @@ const addVideoToPlaylist = asyncHandler(async (req, res) => {
 
   if (!updatePlaylist) throw new ApiError(400, "Something went wrong!!!");
 
-  return res.status(201).json(new ApiResponse(200, updatedPlaylist));
+  return res
+    .status(201)
+    .json(new ApiResponse(200, updatedPlaylist, "Video added..."));
 });
 
+// remove video from playlist
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
   const { playlistId, videoId } = req.params;
-  // TODO: remove video from playlist
+
+  const playlist = await Playlist.findById(playlistId);
+
+  if (!playlist) throw new ApiError(404, "Playlist not found!!!");
+
+  const video = await Video.findById(videoId);
+
+  if (!video) throw new ApiError(404, "Video not found!!!");
+
+  if (!playlist?.owner.equals(req.user?._id))
+    throw new ApiError(401, "Unauthorized request!!!");
+
+  const updatedPlaylist = await Playlist.findByIdAndUpdate(
+    playlistId,
+    {
+      $pull: { videos: videoId },
+    },
+    { new: true }
+  );
+
+  if (!updatePlaylist) throw new ApiError(400, "Something went wrong!!!");
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, updatedPlaylist, "Video removed..."));
 });
 
+// delete playlist
 const deletePlaylist = asyncHandler(async (req, res) => {
   const { playlistId } = req.params;
-  // TODO: delete playlist
+
+  const playlist = await Playlist.findById(playlistId);
+
+  if (!playlist) throw new ApiError(404, "Playlist not found!!!");
+
+  await Playlist.findByIdAndDelete(playlistId);
+
+  return res.status(200).json(new ApiResponse(200, {}, "Playlist deleted..."));
 });
 
+// update playlist
 const updatePlaylist = asyncHandler(async (req, res) => {
   const { playlistId } = req.params;
   const { name, description } = req.body;
-  //TODO: update playlist
+
+  const playlist = await Playlist.findById(playlistId);
+
+  if (!playlist) throw new ApiError(404, "Playlist not found!!!");
+
+  const updatedPlaylistDetails = await Playlist.findByIdAndUpdate(
+    playlistId,
+    {
+      $set: {
+        name,
+        description,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!updatedPlaylistDetails)
+    throw new ApiError(400, "Something went wrong!!!");
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, updatedPlaylistDetails, "Playlist updated..."));
 });
 
 export {
