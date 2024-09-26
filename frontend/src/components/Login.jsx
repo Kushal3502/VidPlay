2e3f1fb1e64f1889b77a9dec7179aaa9fae85e7a
Kushal3502 <kushal2002personal@gmail.com>
2024-09-26 16:08:13 +0530

    login ad register component

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -37,47 +37,77 @@ function Login() {
   };
 
   return (
-    <div className="">
-      <h2>Welcome back!!!</h2>
-      <p>
-        Don't have an account?
-        <Link to={"/auth/register"}>Register</Link>
-      </p>
-      <form onSubmit={handleSubmit(handleLogin)}>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="email">Email</Label>
-          <Input
-            type="email"
-            id="email"
-            placeholder="Email"
-            className="text-black"
-            {...register("email", {
-              pattern: {
-                value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-                message: "Invalid email address!!!",
-              },
-            })}
-          />
-          {errors.email && <div>{errors.email.message}</div>}
-        </div>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="password">Password</Label>
-          <Input
-            type="password"
-            id="password"
-            placeholder="Password"
-            className="text-black"
-            {...register("password", {
-              minLength: {
-                value: 5,
-                message: "Password must be atleast 6 characters long!!!",
-              },
-            })}
-          />
-          {errors.password && <div>{errors.password.message}</div>}
-        </div>
-        <Button variant="secondary">Login</Button>
-      </form>
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <div className="p-8 shadow-md rounded-xl max-w-screen-md w-full border-2 border-white">
+        <h2 className="text-4xl font-semibold font-mono mb-8">
+          Welcome back🫦💦
+        </h2>
+        <form
+          onSubmit={handleSubmit(handleLogin)}
+          className="grid grid-cols-1 gap-5"
+        >
+          <div className="flex justify-center items-center gap-4">
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                type="email"
+                id="email"
+                placeholder="Email"
+                className="text-black"
+                {...register("email", {
+                  pattern: {
+                    value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+                    message: "Invalid email address!!!",
+                  },
+                })}
+              />
+              {errors.email && <div>{errors.email.message}</div>}
+            </div>
+            <p>or,</p>
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="username">Username</Label>
+              <Input
+                type="text"
+                id="username"
+                placeholder="Username"
+                className="text-black"
+                {...register("username")}
+              />
+              {errors.username && <div>{errors.username.message}</div>}
+            </div>
+          </div>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              id="password"
+              placeholder="Password"
+              className="text-black w-4/5"
+              {...register("password", {
+                minLength: {
+                  value: 5,
+                  message: "Password must be atleast 6 characters long!!!",
+                },
+              })}
+            />
+            {errors.password && <div>{errors.password.message}</div>}
+          </div>
+          <div className="flex justify-between items-center">
+            <Button
+              variant="destructive"
+              className="w-3/6 bg-amber-500 hover:bg-amber-600 text-black font-semibold text-base"
+            >
+              Login
+            </Button>
+            <p className="flex justify-center items-center pr-10">
+              Don't have an account?
+              <Link to={"/auth/register"} className="text-blue-500 underline">
+                Register
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -5,18 +5,17 @@ import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { Loader2 } from "lucide-react";
 
 function Register() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm();
   const navigate = useNavigate();
 
   const handleRegister = async (data) => {
-    console.log(data);
-
     let userInfo = new FormData();
 
     userInfo.append("username", data.username);
@@ -30,10 +29,6 @@ function Register() {
       method: "post",
       maxBodyLength: Infinity,
       url: "http://127.0.0.1:8000/api/v1/users/register",
-      headers: {
-        Cookie:
-          "accessToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmViYjUyMjIyZTIzNmRmYWJmNGQwNWMiLCJlbWFpbCI6InVzZXIxQGdtYWlsLmNvbSIsInVzZXJuYW1lIjoidXNlcjEiLCJmdWxsbmFtZSI6Ik1yLiBVc2VyMSIsImlhdCI6MTcyNzI0ODE2MSwiZXhwIjoxNzI3MzM0NTYxfQ.vm2losS033ck6nRYUhhtT1dg5PLtWL2-CmCj06KBRjE; refreshToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmViYjUyMjIyZTIzNmRmYWJmNGQwNWMiLCJpYXQiOjE3MjcyNDgxNjEsImV4cCI6MTcyODExMjE2MX0.sKAtdRPiuOl7MowWeK-0dSjUobCZP6I9hUqECDNqLA0",
-      },
       data: userInfo,
     };
 
@@ -47,82 +42,155 @@ function Register() {
   };
 
   return (
-    <div className="">
-      <h2>Welcome to VidPlay!!!</h2>
-      <p>
-        Already have an account?
-        <Link to={"/auth/login"}>Login</Link>
-      </p>
-      <form onSubmit={handleSubmit(handleRegister)}>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="username">Username</Label>
-          <Input
-            type="text"
-            id="username"
-            placeholder="Username"
-            className="text-black"
-            {...register("username", {
-              minLength: {
-                value: 3,
-                message: "Username must be atleast 3 characters long!!!",
-              },
-            })}
-          />
-          {errors.username && <div>{errors.username.message}</div>}
-        </div>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="email">Email</Label>
-          <Input
-            type="email"
-            id="email"
-            placeholder="Email"
-            className="text-black"
-            {...register("email", {
-              pattern: {
-                value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-                message: "Invalid email address!!!",
-              },
-            })}
-          />
-          {errors.email && <div>{errors.email.message}</div>}
-        </div>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="fullname">Fullname</Label>
-          <Input
-            type="text"
-            id="fullname"
-            placeholder="Fullname"
-            className="text-black"
-            {...register("fullname")}
-          />
-          {errors.fullname && <div>{errors.fullname.message}</div>}
-        </div>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="avatar">Avatar</Label>
-          <Input id="avatar" type="file" {...register("avatar")} />
-        </div>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="coverImage">Cover Image</Label>
-          <Input id="coverImage" type="file" {...register("coverImage")} />
-        </div>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="password">Password</Label>
-          <Input
-            type="password"
-            id="password"
-            placeholder="Password"
-            className="text-black"
-            {...register("password", {
-              minLength: {
-                value: 5,
-                message: "Password must be atleast 6 characters long!!!",
-              },
-            })}
-          />
-          {errors.password && <div>{errors.password.message}</div>}
-        </div>
-        <Button variant="secondary">Register</Button>
-      </form>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <div className="p-8 shadow-md rounded-xl max-w-screen-md w-full border-2 border-white">
+        <h2 className="text-4xl font-semibold font-mono mb-8">
+          Welcome to VidPlay✨
+        </h2>
+        <form
+          onSubmit={handleSubmit(handleRegister)}
+          className="grid grid-cols-2 gap-5"
+        >
+          {/* Fullname */}
+          <div className="grid gap-1.5">
+            <Label htmlFor="fullname" className="">
+              Fullname
+            </Label>
+            <Input
+              type="text"
+              id="fullname"
+              placeholder="Fullname"
+              className="text-black "
+              {...register("fullname")}
+            />
+          </div>
+          {/* Username */}
+          <div className="grid gap-1.5">
+            <Label htmlFor="username" className="">
+              Username
+            </Label>
+            <Input
+              type="text"
+              id="username"
+              placeholder="Username"
+              className={`text-black ${
+                errors.username ? "border-red-500" : ""
+              }`}
+              {...register("username", {
+                minLength: {
+                  value: 3,
+                  message: "Username must be at least 3 characters long!",
+                },
+              })}
+            />
+            {errors.username && (
+              <span className="text-red-500 text-sm">
+                {errors.username.message}
+              </span>
+            )}
+          </div>
+
+          {/* Email */}
+          <div className="grid gap-1.5">
+            <Label htmlFor="email" className="">
+              Email
+            </Label>
+            <Input
+              type="email"
+              id="email"
+              placeholder="Email"
+              className={`text-black ${errors.email ? "border-red-500" : ""}`}
+              {...register("email", {
+                pattern: {
+                  value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+                  message: "Invalid email address!",
+                },
+              })}
+            />
+            {errors.email && (
+              <span className="text-red-500 text-sm">
+                {errors.email.message}
+              </span>
+            )}
+          </div>
+
+          {/* Password */}
+          <div className="grid gap-1.5">
+            <Label htmlFor="password" className="">
+              Password
+            </Label>
+            <Input
+              type="password"
+              id="password"
+              placeholder="Password"
+              className={`text-black ${
+                errors.password ? "border-red-500" : ""
+              }`}
+              {...register("password", {
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters long!",
+                },
+              })}
+            />
+            {errors.password && (
+              <span className="text-red-500 text-sm">
+                {errors.password.message}
+              </span>
+            )}
+          </div>
+
+          {/* Avatar */}
+          <div className="grid gap-1.5">
+            <Label htmlFor="avatar" className="">
+              Avatar
+            </Label>
+            <Input
+              id="avatar"
+              type="file"
+              {...register("avatar")}
+              className="cursor-pointer"
+            />
+          </div>
+
+          {/* Cover Image */}
+          <div className="grid gap-1.5">
+            <Label htmlFor="coverImage" className="">
+              Cover Image
+            </Label>
+            <Input
+              id="coverImage"
+              type="file"
+              {...register("coverImage")}
+              className="cursor-pointer"
+            />
+          </div>
+
+          {/* Register Button */}
+          {isSubmitting ? (
+            <Button
+              disabled
+              className=" bg-amber-500 hover:bg-amber-600 text-black font-semibold text-base"
+            >
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Please wait
+            </Button>
+          ) : (
+            <Button
+              variant="destructive"
+              className="w-full bg-amber-500 hover:bg-amber-600 text-black font-semibold text-base"
+            >
+              Register
+            </Button>
+          )}
+          <p className="flex justify-center items-center">
+            Already have an account?
+            <Link to={"/auth/login"} className="text-blue-500 underline">
+              Login
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
