c63cb367155c3924d87e136c99fd23d3a8a0be5f
Kushal3502 <kushal2002personal@gmail.com>
2024-10-16 23:50:39 +0530

    added loading state

--- frontend/src/pages/home/Tweets.jsx
+++ frontend/src/pages/home/Tweets.jsx
@@ -1,42 +1,43 @@
 import { useEffect, useState } from "react";
-import { TweetCard, Videos } from "../../components";
-import axios from "axios";
+import { TweetCard } from "../../components";
+import { get } from "@/utils/api";
+import { ScaleLoader } from "react-spinners";
 
 function Tweets() {
   const [tweets, setTweets] = useState([]);
 
-  const fetchVideos = async () => {
-    try {
-      const response = await axios.get("http://127.0.0.1:8000/api/v1/tweets/", {
-        withCredentials: true,
-      });
-      console.log(response.data);
-      setTweets(response.data.data);
-    } catch (error) {
-      console.log("Video fetch error :: ", error);
-    }
+  const fetchTweets = async () => {
+    const response = await get("/tweets/");
+    console.log(response);
+    setTweets(response.data);
   };
 
   useEffect(() => {
-    fetchVideos();
+    fetchTweets();
   }, []);
 
   return (
     <div className="p-4 sm:p-6 lg:px-10">
       <h2 className="lg:text-3xl mb-4">Popular tweets</h2>
-      <div className="">
-        {tweets && tweets.length > 0 ? (
-          <div className="grid lg:grid-cols-4 sm:grid-col-2 grid-col-1 gap-8">
-            {tweets.map((tweet) => (
-              <div key={tweet._id} className=" cursor-pointer">
-                <TweetCard tweet={tweet} />
-              </div>
-            ))}
-          </div>
-        ) : (
-          <p>No tweet available</p>
-        )}
-      </div>
+      {tweets ? (
+        <div className="">
+          {tweets.length > 0 ? (
+            <div className="grid lg:grid-cols-4 sm:grid-col-2 grid-col-1 gap-8">
+              {tweets.map((tweet) => (
+                <div key={tweet._id} className=" cursor-pointer">
+                  <TweetCard tweet={tweet} />
+                </div>
+              ))}
+            </div>
+          ) : (
+            <p>No tweet available</p>
+          )}
+        </div>
+      ) : (
+        <div className="flex justify-center items-center h-screen">
+          <ScaleLoader color="#ffffff" />
+        </div>
+      )}
     </div>
   );
 }
